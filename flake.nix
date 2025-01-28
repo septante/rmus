007bb6d02b53118512bbc32ff5ce8c9a80999bd3
septante <130835353+septante@users.noreply.github.com>
2025-01-28 15:50:47 -0800

    add initial files

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    ...
+  }:
+  let
+    overlays = [ (import rust-overlay) ];
+
+    systems = [
+      "x86_64-linux"
+    ];
+
+    eachSystem = f:
+      nixpkgs.lib.genAttrs systems
+      (system: f { pkgs = import nixpkgs { inherit system overlays; }; });
+  in
+  {
+    devShells = eachSystem ({ pkgs }: with pkgs; {
+      default = mkShell rec {
+        buildInputs = [
+          rust-bin.stable.latest.default
+        ];
+        LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+      };
+    });
+  };
+}
