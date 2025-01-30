9835360c531c8213953a56e24296db20f08e76cc
septante <130835353+septante@users.noreply.github.com>
2025-01-29 22:38:47 -0800

    feat(library): add ability to import library and read tags

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::{fs, path::Path};
+use std::borrow::Cow;
+use std::{fs, path::PathBuf};
 
 use cursive::{traits::*, views::Dialog};
 use cursive_table_view::{TableView, TableViewItem};
 use dirs;
+use lofty::prelude::*;
+use lofty::probe::Probe;
 
 #[non_exhaustive]
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
@@ -14,15 +17,58 @@ enum Field {
 #[non_exhaustive]
 #[derive(Clone, Debug)]
 struct Track {
-    title: String,
-    artist: String,
+    path: PathBuf,
+    metadata: Metadata,
+}
+
+impl TryFrom<PathBuf> for Track {
+    type Error = anyhow::Error;
+
+    fn try_from(path: PathBuf) -> Result<Self, Self::Error> {
+        let tagged_file = Probe::open(&path)?.read()?;
+        let tag = match tagged_file.primary_tag() {
+            Some(primary_tag) => primary_tag,
+            // If the "primary" tag doesn't exist, we just grab the
+            // first tag we can find. Realistically, a tag reader would likely
+            // iterate through the tags to find a suitable one.
+            None => tagged_file.first_tag().expect("ERROR: No tags found!"),
+        };
+        Ok(Track {
+            path,
+            metadata: Metadata {
+                title: Metadata::tag_to_string(tag.title()),
+                artist: Metadata::tag_to_string(tag.artist()),
+            },
+        })
+    }
+}
+
+#[non_exhaustive]
+#[derive(Clone, Debug)]
+struct Metadata {
+    title: Option<String>,
+    artist: Option<String>,
+}
+
+impl Metadata {
+    fn tag_to_string(tag: Option<Cow<str>>) -> Option<String> {
+        tag.as_deref().map(|x| x.to_owned())
+    }
 }
 
 impl TableViewItem<Field> for Track {
     fn to_column(&self, column: Field) -> String {
         match column {
-            Field::Title => self.title.clone(),
-            Field::Artist => self.artist.clone(),
+            Field::Title => self
+                .metadata
+                .title
+                .clone()
+                .unwrap_or("Unknown Title".to_owned()),
+            Field::Artist => self
+                .metadata
+                .artist
+                .clone()
+                .unwrap_or("Unknown Artist".to_owned()),
         }
     }
 
@@ -31,8 +77,8 @@ impl TableViewItem<Field> for Track {
         Self: Sized,
     {
         match column {
-            Field::Title => self.title.cmp(&other.title),
-            Field::Artist => self.artist.cmp(&other.artist),
+            Field::Title => self.metadata.title.cmp(&other.metadata.title),
+            Field::Artist => self.metadata.artist.cmp(&other.metadata.artist),
         }
     }
 }
@@ -41,17 +87,10 @@ fn main() {
     let library_root = dirs::audio_dir().expect("couldn't find music folder");
     let files = fs::read_dir(library_root)
         .expect("Error reading directory")
-        .filter(|x| {
-            x.as_ref()
-                .expect("Error getting directory entry")
-                .file_type()
-                .expect("Error getting file type")
-                .is_file()
-        });
+        .flatten()
+        .filter(|x| x.file_type().expect("Error getting file type").is_file());
 
-    for f in files {
-        println!("Name: {}", f.unwrap().path().display());
-    }
+    let tracks = files.map(|f| Track::try_from(f.path())).flatten().collect();
 
     let mut siv = cursive::default();
 
@@ -59,13 +98,7 @@ fn main() {
         .column(Field::Title, "Title", |c| c.width_percent(20))
         .column(Field::Artist, "Artist", |c| c.width_percent(20));
 
-    let mut sample_data = Vec::new();
-    sample_data.push(Track {
-        title: "Song1".to_owned(),
-        artist: "Artist1".to_owned(),
-    });
-
-    table.set_items(sample_data);
+    table.set_items(tracks);
 
     table.set_on_submit(|siv, row, index| {
         // Play song
